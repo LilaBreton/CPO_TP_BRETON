ddacfbb74805ef34fedc0cc8e844cb9d46a35db6
Lila Breton <lila.breton@epfedu.fr>
2024-09-17 16:57:21 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Breton/Exo 1/src/exo/pkg1/Exo1.java
@@ -0,0 +1,30 @@
+/* Lila Breton
+ * TDC
+ * TP0 Exercice 1
+ * 17 Septembre 2024
+ */
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lilab
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine() ;
+        // TODO code application logic here
+    }
+    
+}
