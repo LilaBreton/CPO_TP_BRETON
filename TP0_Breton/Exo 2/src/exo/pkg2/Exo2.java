10f9d000b66c7122a9ce1735a6e634e7d018d1d3
Lila Breton <lila.breton@epfedu.fr>
2024-09-17 17:09:22 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Breton/Exo 2/src/exo/pkg2/Exo2.java
@@ -0,0 +1,37 @@
+/* Lila Breton
+ * TDC
+ * TP0 Exercice 1
+ * 17 Septembre 2024
+ */
+package exo.pkg2;
+
+/**
+ *
+ * @author lilab
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        nb=5;
+        result=0;
+        
+        // Addition des nb premiers entiers
+        ind=1;
+        while (ind <= nb) {
+            result=result+ind;
+        }
+        
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
+        
+    }
+    
+}
