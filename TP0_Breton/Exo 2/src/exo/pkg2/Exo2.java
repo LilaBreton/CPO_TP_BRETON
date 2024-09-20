f3f620ec70d15263913b837662def04b0b285d40
Lila Breton <lila.breton@epfedu.fr>
2024-09-20 12:30:24 +0200

    Création d'un nouveau projet versionné

--- TP0_Breton/Exo 2/src/exo/pkg2/Exo2.java
+++ TP0_Breton/Exo 2/src/exo/pkg2/Exo2.java
@@ -1,9 +1,11 @@
 /* Lila Breton
  * TDC
- * TP0 Exercice 1
- * 17 Septembre 2024
+ * TP0 Exercice 2
+ * 17 Septembre 2024-20 Septembre 2024
  */
 package exo.pkg2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -19,13 +21,18 @@ public class Exo2 {
         int nb; // nombre dentiers a additionner
         int result; // resultat
         int ind; //indice
-        nb=5;
+        //nb=5;
         result=0;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
         
         // Addition des nb premiers entiers
         ind=1;
         while (ind <= nb) {
             result=result+ind;
+            ind = ind + 1;
         }
         
         // Affichage du resultat
