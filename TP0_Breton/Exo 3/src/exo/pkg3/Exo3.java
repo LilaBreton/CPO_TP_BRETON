549b906083c6076ab139e30313ee89ba050cf180
Lila Breton <lila.breton@epfedu.fr>
2024-09-20 12:45:50 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Breton/Exo 3/src/exo/pkg3/Exo3.java
@@ -0,0 +1,40 @@
+/*Lila Breton
+* TDC
+* TP0 Exercice 3
+* 20 Septembre 2024
+ */
+package exo.pkg3;
+
+/**
+ *
+ * @author lilab
+ */
+public class Exo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:");
+        System.out.println("1) Add");
+        System.out.println("2) Substract");
+        System.out.println("3) Multiply");
+        System.out.println("4) Divide");
+        System.out.println("5) Modulo");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
