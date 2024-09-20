97456abb3cee6db066d41a7f7cde9a125e0fda5a
Lila Breton <lila.breton@epfedu.fr>
2024-09-20 13:47:23 +0200

    Création d'un nouveau projet versionné

--- TP0_Breton/Exo 3/src/exo/pkg3/Exo3.java
+++ TP0_Breton/Exo 3/src/exo/pkg3/Exo3.java
@@ -5,6 +5,8 @@
  */
 package exo.pkg3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lilab
@@ -15,6 +17,20 @@ public class Exo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int operateur; 
+        int operande1;
+        int operande2;
+        int result;
+        
+        operateur = 0;
+        operande1 = 0;
+        operande2 = 0;
+        result = 0;
+        
+        
         System.out.println("Please enter the operator:");
         System.out.println("1) Add");
         System.out.println("2) Substract");
@@ -22,38 +38,40 @@ public class Exo3 {
         System.out.println("4) Divide");
         System.out.println("5) Modulo");
         
-        int operateur; 
-        int operande1;
-        int operande2;
-        int result;
-        
-        System.out.println("Veuillez entrer un chiffre :");
         operateur = sc.nextInt();
-        System.out.println("Veuillez entrer un nombre :");
+        if (operateur<1 && operateur>5){
+            System.out.println("Please enter the operator:");
+        }
+        
+        
+        System.out.println("Please enter the first number:");
         operande1 = sc.nextInt();
-        System.out.println("Veuillez entrer un nombre :");
+        System.out.println("Please enter the second number:");
         operande2 = sc.nextInt();
         
-        if (operateur=1){
-            result = operande1 + operande2
+        
+        if (operateur==1){
+            result = operande1 + operande2;
         }
         
-        if (operateur=2){
-            result = operande1 - operande2
+        if (operateur==2){
+            result = operande1 - operande2;
         } 
         
-        if (operateur=3){
-            result = operande1 * operande2
+        if (operateur==3){
+            result = operande1 * operande2;
         }
         
-        if (operateur=4){
-            result = operande1 / operande2
+        if (operateur==4){
+            result = operande1 / operande2;
         }
         
-        if (operateur=5){
-            result = operande1 % operande2
+        if (operateur==5){
+            result = operande1 % operande2;
         }
         
+        
+        System.out.println("The result is :"+ result);
         
     }
     
