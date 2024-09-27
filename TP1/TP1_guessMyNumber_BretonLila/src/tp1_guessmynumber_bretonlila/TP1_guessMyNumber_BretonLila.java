8720126f81572f0a94aba7af8b981cff6abe212e
Lila Breton <lila.breton@epfedu.fr>
2024-09-27 15:31:14 +0200

    Création d'un nouveau projet versionné

---
+++ TP1/TP1_guessMyNumber_BretonLila/src/tp1_guessmynumber_bretonlila/TP1_guessMyNumber_BretonLila.java
@@ -0,0 +1,50 @@
+/*Lila Breton
+* TDC
+* TP1 Exercice 3
+* 27 Septembre 2024
+ */
+package tp1_guessmynumber_bretonlila;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lilab
+ */
+public class TP1_guessMyNumber_BretonLila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random generateurAleat = new Random();
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 1; i<=5; i++){
+            int n = generateurAleat.nextInt(100);
+            System.out.print(n + " ");
+        }
+        
+        int nb = generateurAleat.nextInt(100);
+        System.out.println("Veuillez saisir un nombre entre 0 et 100 :");
+        int nombre = sc.nextInt();
+        
+        while (nombre != nb) {
+            if (nb>nombre){
+                System.out.println("Trop petit");
+                System.out.println("Veuillez saisir un nombre plus grand :");
+                nombre = sc.nextInt();
+            }
+            else if (nb<nombre){
+                System.out.println("Trop grand");
+                System.out.println("Veuillez saisir un nombre plus petit :");
+                nombre = sc.nextInt();
+            }
+            else {
+                System.out.println("Gagne");
+                //System.out.println("Vous avez effectue " + compteur + " tentatives");
+            }
+        }
+    }
+}
