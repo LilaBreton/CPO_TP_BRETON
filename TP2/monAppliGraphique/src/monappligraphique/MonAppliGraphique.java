091bd9ff820403f279e1bb7fcb1990f97a01d461
Lila Breton <lila.breton@epfedu.fr>
2024-10-16 12:30:55 +0200

    Création d'un nouveau projet versionné

---
+++ TP2/monAppliGraphique/src/monappligraphique/MonAppliGraphique.java
@@ -0,0 +1,21 @@
+/*Lila Breton
+* TDC
+* TP2 Exercice 1
+* 16 Octobre 2024
+ */
+package monappligraphique;
+
+/**
+ *
+ * @author lilab
+ */
+public class MonAppliGraphique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fenetre1 f = new Fenetre1();
+        f.show();
+    }
+}
